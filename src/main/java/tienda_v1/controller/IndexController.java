ef8f2e72afa603eaa57ab632465485b3745e166e
jgarcia60436 <jgarcia60436@ufide.ac.cr>
2023-03-11 14:22:29 -0600

    Semana 8

---
+++ src/main/java/tienda_v1/controller/IndexController.java
@@ -0,0 +1,15 @@
+package tienda_v1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        return "index";
+    }
+
+}
